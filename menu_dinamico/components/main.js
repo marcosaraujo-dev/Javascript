6cef2472e2f228b4bd80b919e7ad4658cc4cd25d
marcosaraujo-dev <64809651+marcosaraujo-dev@users.noreply.github.com>
2021-08-02 15:37:37 -0300

    Menu dinâmico com valores recuperados de um Json

---
+++ menu_dinamico/components/main.js
@@ -0,0 +1,17 @@
+﻿import Tree from './tree.js'
+
+const menu = [
+    { id: 8, name: "Gestão Sistema", parent: null, iconeItem: "fa-circle-o-notch", urlItem: null},
+    { id: 18, name: "Gestão de Clientes", parent: null, iconeItem: "fa-users", urlItem: null },
+    { id: 3, name: "Cadastros Internos", parent: 8, iconeItem: null, urlItem: null },
+    { id: 1, name: "Permissões", parent: 3, iconeItem: null, urlItem: "#" },
+    { id: 4, name: "Emails", parent: 8, iconeItem: null, urlItem: "#"},
+    { id: 6, name: "CEP", parent: 3, iconeItem: null, urlItem: "#" },
+    { id: 7, name: "Clientes", parent: 18, iconeItem: null, urlItem: null },
+    { id: 10, name: "Processo e controles", parent: 18, iconeItem: null, urlItem: null },
+    { id: 20, name: 'Desligamentos', parent: 10, iconeItem: null, urlItem: "#" },
+    { id: 13, name: "Termos e contratos", parent: 18, iconeItem: null, urlItem: null },
+    { id: 14, name: "Termos Restritos", parent: 13, iconeItem: null, urlItem: "#" }
+]
+
+Tree(menu)
---
+++ menu_dinamico/components/tree.js
@@ -0,0 +1,80 @@
+export default function(data) {
+    // pega a tag principal que irá receber o menu
+    const tree = document.querySelector('nav#tree')
+
+    const tree2 = document.querySelector('div#tree')
+
+    // recebe toda a arvore de elementos
+    const menu = document.createElement('ul')
+    menu.classList.add("nav")
+    menu.classList.add("nav-list")
+    menu.classList.add("flex-column")
+    const firstLevel = data.filter(item => !item.parent)
+    const getFirstLis = firstLevel.map(buildTree) // retorno novo array com li's
+    getFirstLis.forEach( li => menu.append(li)) // adicionar li's ao menu
+
+
+    function buildTree(item) {
+        
+        let menuPrincipal = ''
+        if (!item.parent) {
+           
+            menuPrincipal += '<i class="fa ' + item.iconeItem+' " ></i >'
+            menuPrincipal += '<span class="menu-text" > ' + item.name + '</span > '
+            //   menuPrincipal += '<b class="arrow fa fa-angle-down" ></b ></a >'
+        }
+        else {
+            menuPrincipal = item.name
+
+        }
+
+     
+
+
+        // primeiro elemento
+        const li = document.createElement('li')
+        li.innerHTML = menuPrincipal
+       // li.innerHTML = item.name //menuPrincipal
+        li.classList.add("nav-item")
+
+       
+         
+        const children = data.filter(child => child.parent === item.id)
+
+        if (children.length > 0) {
+
+            //adiciona um click para os parents
+            li.addEventListener('click', event => {
+                event.stopPropagation()
+                event.target.classList.toggle('open')
+            })
+
+            // adiciona uma classe identificadora de que tem filhos
+            li.classList.add('has-children')
+
+            // constroi os filhos
+            const subMenu = document.createElement('ul')
+            subMenu.classList.add('submenu')
+            children.map(buildTree)
+                .forEach(li => subMenu.append(li))
+            li.append(subMenu)
+        }
+        else {
+            let linkAcesso = ''
+           
+            linkAcesso = '<a class="" href="' + item.urlItem +'" >'
+            linkAcesso += '<span class="menu-text" > ' + item.name + '</span > '
+            linkAcesso += '</a >'
+
+            li.innerHTML = linkAcesso
+        }
+       
+        
+        // adicionar os elements ao menu
+        return li
+    }
+
+    // adiciona o menu no HTML
+    tree.append(menu)
+   // tree2.append(menu)
+}
